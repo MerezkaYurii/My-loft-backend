6753013973752b97394bf1437a19d41d3fb44f84
MerezkaYurii <shkolasad@ukr.net>
2025-07-19 19:15:48 +0300

    post

--- src/app/domain/loft/LoftController.ts
+++ src/app/domain/loft/LoftController.ts
@@ -1,7 +1,4 @@
-import { JsonController, Get, UseAfter } from 'routing-controllers';
-import { HTTPResponseLogger } from '@app/middlewares/HTTPResponseLogger';
-import { ApiResponse } from 'helpers/ApiResponse';
-
+import { JsonController, Get, Post, Body } from 'routing-controllers';
 import {
   getMyPhotos,
   getPhotosFromInternet,
@@ -9,44 +6,81 @@ import {
   getVideosFromInternet,
   getMyEquipment,
   getHowToDoIt,
+  createMyPhoto,
+  createPhotosFromInternet,
+  createMyVideos,
+  createVideosFromInternet,
+  createMyEquipment,
+  createHowToDoIt,
 } from 'db/services/loft.service';
+import { ApiResponse } from 'helpers/ApiResponse';
+import { ILoft } from 'db/models/Loft';
 
 @JsonController('/loft')
 export default class LoftController {
   @Get('/my-photos')
-  @UseAfter(HTTPResponseLogger)
-  async myPhotos() {
+  async getMyPhotosList() {
     const data = await getMyPhotos();
     return new ApiResponse(true, data);
   }
 
-  @Get('/photos-from-internet')
-  async photosFromInternet() {
+  @Get('/internet-photos')
+  async getPhotosFromInternetList() {
     const data = await getPhotosFromInternet();
     return new ApiResponse(true, data);
   }
 
   @Get('/my-videos')
-  async myVideos() {
+  async getMyVideosList() {
     const data = await getMyVideos();
     return new ApiResponse(true, data);
   }
 
-  @Get('/videos-from-internet')
-  async videosFromInternet() {
+  @Get('/internet-videos')
+  async getVideosFromInternetList() {
     const data = await getVideosFromInternet();
     return new ApiResponse(true, data);
   }
 
   @Get('/my-equipment')
-  async myEquipment() {
+  async getMyEquipmentList() {
     const data = await getMyEquipment();
     return new ApiResponse(true, data);
   }
 
-  @Get('/how-to-do-it')
-  async howToDoIt() {
+  @Get('/how-to')
+  async getHowToDoItList() {
     const data = await getHowToDoIt();
     return new ApiResponse(true, data);
+  }
+  @Post('/my-photos')
+  async createMyPhoto(@Body() body: ILoft) {
+    const created = await createMyPhoto(body);
+    return { success: true, data: created };
+  }
+  @Post('/internet-photos')
+  async createPhotosFromInternet(@Body() body: ILoft) {
+    const created = await createPhotosFromInternet(body);
+    return { success: true, data: created };
+  }
+  @Post('/my-videos')
+  async createMyVideos(@Body() body: ILoft) {
+    const created = await createMyVideos(body);
+    return { success: true, data: created };
+  }
+  @Post('/internet-videos')
+  async createVideosFromInternet(@Body() body: ILoft) {
+    const created = await createVideosFromInternet(body);
+    return { success: true, data: created };
+  }
+  @Post('/my-equipment')
+  async createMyEquipment(@Body() body: ILoft) {
+    const created = await createMyEquipment(body);
+    return { success: true, data: created };
+  }
+  @Post('/how-to')
+  async createHowToDoIt(@Body() body: ILoft) {
+    const created = await createHowToDoIt(body);
+    return { success: true, data: created };
   }
 }
--- src/db/services/loft.service.ts
+++ src/db/services/loft.service.ts
@@ -2,6 +2,7 @@
 
 import {
   howToDoItModel,
+  ILoft,
   myEquipmentModel,
   myPhotoModel,
   myVideoModel,
@@ -15,3 +16,22 @@ export const getMyVideos = () => myVideoModel.find().lean();
 export const getVideosFromInternet = () => videoFromInternetModel.find().lean();
 export const getMyEquipment = () => myEquipmentModel.find().lean();
 export const getHowToDoIt = () => howToDoItModel.find().lean();
+
+export const createMyPhoto = (data: Partial<ILoft>) => {
+  return myPhotoModel.create(data);
+};
+export const createPhotosFromInternet = (data: Partial<ILoft>) => {
+  return photoFromInternetModel.create(data);
+};
+export const createMyVideos = (data: Partial<ILoft>) => {
+  return myVideoModel.create(data);
+};
+export const createVideosFromInternet = (data: Partial<ILoft>) => {
+  return videoFromInternetModel.create(data);
+};
+export const createMyEquipment = (data: Partial<ILoft>) => {
+  return myEquipmentModel.create(data);
+};
+export const createHowToDoIt = (data: Partial<ILoft>) => {
+  return howToDoItModel.create(data);
+};
